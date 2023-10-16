8635a854429e7733363ff9002cb5b333a8b2fcbb
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-16 15:51:24 -0500

    Added a post request

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello World!!');
 });
+
+app.post("/",(req,res)=>{
+    console.log(req);
+    res.json({message:'data recieved',data:req.body});
+});
+
 app.listen(5100,()=>{
     console.log('Server is running...');
 })
+
+
