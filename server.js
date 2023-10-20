9cd4c9662eea9336473a7e0b4e6bce6f99bec5fa
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-19 22:41:26 -0500

    Added a route for delete job

--- server.js
+++ server.js
@@ -68,6 +68,20 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
     job.position = position
     res.status(200).json({msg : 'job modified', job});
   });
+
+
+//Delete job
+app.delete('/api/v1/jobs/:id',(req,res)=>{
+  const {id} = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: 'job deleted' });
+});
 const port = process.env.PORT || 5100;
 
 app.listen(5100, () => {
