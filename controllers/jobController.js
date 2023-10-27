a8e0274160f05ecc36e8ca74bcd9766adcfb729a
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-26 22:19:46 -0500

    Added for controllers for get all, get single job,delete job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,10 +5,13 @@ let jobs = [
   { id: nanoid(), company: "google", position: "back end" },
 ];
 
+//Get all Jobs
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
+//Create a job
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
 
@@ -16,12 +19,11 @@ export const createJob = async (req, res) => {
   res.status(201).json({ job });
 };
 
-
+//Get a single job
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
-    // throw new Error('no job with that id');
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
   res.status(200).json({ job });
@@ -41,15 +43,13 @@ export const editJob = async (req, res) => {
   job.position = position;
   res.status(200).json({ msg: "job modified", job });
 };
-
+//Delete a Job
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const removedJob = await Job.findByIdAndDelete(id);
+
+  if (!removedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-
-  res.status(200).json({ msg: "job deleted" });
+  res.status(200).json({ job: removedJob });
 };
