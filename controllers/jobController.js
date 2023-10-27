9bf30061e5c0f64044bb49464f7fff059ab32ee0
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-26 22:32:44 -0500

    Added controller for edit job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -29,19 +29,19 @@ export const getJob = async (req, res) => {
   res.status(200).json({ job });
 };
 
+//Update Job
 export const editJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: "no job with id" });
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedJob) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
   }
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: "job modified", job });
+
+  res.status(200).json({ job: updatedJob });
 };
 //Delete a Job
 export const deleteJob = async (req, res) => {
