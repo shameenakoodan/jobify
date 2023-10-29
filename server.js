37a4ba4f70eed00681a4b58c463a16a429cfe67e
Shameena Koodan <shameena.koodan@gmail.com>
2023-10-28 23:25:21 -0500

    Added middleware validation and constants

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
-import 'express-async-errors';
+import "express-async-errors";
 const app = express();
-
 //routers
-import jobRouter from './routes/jobRouter.js';
-
+import jobRouter from "./routes/jobRouter.js";
+import { validateTest } from "./middleware/validationMiddleware.js";
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 
@@ -24,26 +23,29 @@ app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data recieved", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  validateTest,
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}`, data: req.body });
+  }
+);
 
-app.use('/api/v1/jobs',jobRouter);
+app.use("/api/v1/jobs", jobRouter);
 
 //Not Found Message
-app.use('*',(req,res)=>{
-  res.status(404).json({msg:'not found'})
-})
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
 
 app.use(errorHandlerMiddleware);
 //Error Message
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: 'something went wrong' });
+  res.status(500).json({ msg: "something went wrong" });
 });
 const port = process.env.PORT || 5100;
-
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
@@ -54,4 +56,3 @@ try {
   console.log(error);
   process.exit(1);
 }
-
